21b696ba280a6687f6dede6f1197c904a7f33237
ashwininboyite <32223555+ashwininboyite@users.noreply.github.com>
2021-04-12 16:18:37 +0530

    Add files via upload

---
+++ concept drift_linear_regression.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import ks_2samp
+from scipy.stats import binom_test
+
+#modelop.init
+def begin():
+    global train, actual_values
+    train = pd.read_csv('training_data.csv')
+    actual_values = train.price
+    pass
+
+#modelop.score
+def action(datum):
+    yield datum
+
+#modelop.metrics
+def metrics(data):
+    predicted_values = data.price
+    pvalue = ks_2samp(actual_values, predicted_values)
+    
+    yield {"pvalue": pvalue}
